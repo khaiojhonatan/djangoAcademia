ad5e047174a0f2b83ddfeabde0cbbb2375fdc491
Khaio Jhonatan <khaiojhonatan@gmail.com>
2022-12-11 13:38:26 -0300

    banco de samuel ajeitado (eu acho)

--- alunos/models.py
+++ alunos/models.py
@@ -1,27 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
-class Alunos(models.Model):
-
-    inscricao = models.DateField(auto_now = True,primary_key=True)
-    nome = models.CharField(max_length=50)
-    # sexo
-    nascimento = models.DateField()
-    telefone = models.FloatField()
-    email = models.EmailField()
-    rg = models.FloatField(max_length=11)
-    cpf = models.FloatField()
-    bairro = models.CharField(max_length=40)
-    rua = models.CharField(max_length=80)
-    num_residencia = models.FloatField()
-
-    def __str__(self):
-        return self.nome
-
 class DadosAcademia(models.Model):
-    dat_medidas = models.DateField(primary_key=True)
-    aluno = models.ForeignKey(Alunos, on_delete=models.CASCADE)
+    dat_medidas = models.DateField()
     altura = models.FloatField(blank =True, null= True)
     peso = models.FloatField(blank =True, null= True)
     imc = models.FloatField(blank =True, null= True)
@@ -38,3 +19,21 @@ class DadosAcademia(models.Model):
     biceps_E = models.FloatField(blank =True, null= True)
     coxa_D = models.FloatField(blank =True, null= True)
     coxa_E = models.FloatField(blank =True, null= True)
+
+class Alunos(models.Model):
+
+    inscricao = models.DateField(auto_now = True)
+    nome = models.CharField(max_length=50)
+    # sexo
+    nascimento = models.DateField()
+    telefone = models.CharField(max_length=25)
+    email = models.EmailField()
+    rg = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=16)
+    bairro = models.CharField(max_length=40)
+    rua = models.CharField(max_length=80)
+    num_residencia = models.CharField(max_length=10)
+    dadoscorporais = models.ForeignKey(DadosAcademia, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nome
+
