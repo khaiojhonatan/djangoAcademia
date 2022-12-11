c9f908d85bb30177484a773094201dd482e79117
rngiovanna4 <118777919+rngiovanna4@users.noreply.github.com>
2022-12-11 19:46:26 -0300

    Cobranças

--- academia/views.py
+++ academia/views.py
@@ -5,11 +5,12 @@ def index(request):
     return render(request, 'templates/index.html')
 
 def cobrancas(request, Alunos_id, Alunos_divida):
-    Alunos =Alunos.objects.get(id=Alunos_id)
+    Alunos = Alunos.objects.get(id=Alunos_id, divida=Alunos_divida)
 
     if request.method == "POST" and Alunos_divida == True:
         Alunos_divida = False
         
+
     return render(request,'templates/cobrancas.html', {Alunos_id: Alunos_id, Alunos_divida: Alunos_divida})
 
 
